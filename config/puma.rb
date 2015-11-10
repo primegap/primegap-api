c3173c8a0505113ba62780f01aa7e22b63be4d8e
Luca Tironi <luca.tironi@gmail.com>
2015-11-10 11:40:47 +0100

    Add puma config

---
+++ config/puma.rb
@@ -0,0 +1,22 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 3)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup DefaultRackup
+port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+redis_url = ENV['REDISCLOUD_URL'] || 'redis://localhost:6379'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+
+  if defined?(Resque)
+    Resque.redis = Redis.new(url: redis_url)
+    Rails.logger.info('Resque: Connected to Redis')
+  end
+end
