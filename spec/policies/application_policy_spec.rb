f2d5b3ba60d95d2cfb466ee469a43b3bd86f7ee7
Luca Tironi <luca.tironi@gmail.com>
2015-11-27 23:44:42 +0100

    Add spec for application_policy

---
+++ spec/policies/application_policy_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe ApplicationPolicy do
+  subject { ApplicationPolicy.new(user, record) }
+
+  let(:user) { User.create(email: 'user@example.com', password: 'password') }
+  let(:record) { double(:record) }
+
+  it { should_not permit(:index) }
+  it { should_not permit(:show) }
+  it { should_not permit(:create) }
+  it { should_not permit(:update) }
+  it { should_not permit(:destroy) }
+end
