ab57607413a4fa117f8801a7c9388a0bc68d5d46
Luca Tironi <luca.tironi@gmail.com>
2015-11-28 15:39:28 +0100

    Add pundit's customer_policy to authorize customers only belonging to the same company of the user

---
+++ app/policies/customer_policy.rb
@@ -0,0 +1,34 @@
+class CustomerPolicy < ApplicationPolicy
+  attr_reader :user, :customer
+
+  def initialize(user, customer)
+    @user     = user
+    @customer = customer
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    customer.company_id == user.company_id
+  end
+
+  def create?
+    customer.company_id == user.company_id
+  end
+
+  def update?
+    customer.company_id == user.company_id
+  end
+
+  def destroy?
+    customer.company_id == user.company_id
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.where(company_id: user.company_id)
+    end
+  end
+end
---
+++ spec/policies/customer_policy_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe CustomerPolicy do
+  subject { CustomerPolicy.new(user, customer) }
+
+  let(:user) { User.create(email: 'user@example.com', password: 'password', company_id: company_id) }
+  let(:customer) { Customer.create(full_name: 'John Doe', email: 'john.doe@example.com', phone: '123456789', company_id: 1) }
+
+  context 'for a user of the same company' do
+    let(:company_id) { 1 }
+
+    it { should permit(:index) }
+    it { should permit(:show) }
+    it { should permit(:create) }
+    it { should permit(:update) }
+    it { should permit(:destroy) }
+  end
+
+  context 'for a user of a different company' do
+    let(:company_id) { 2 }
+
+    it { should permit(:index) }
+    it { should_not permit(:show) }
+    it { should_not permit(:create) }
+    it { should_not permit(:update) }
+    it { should_not permit(:destroy) }
+  end
+end
