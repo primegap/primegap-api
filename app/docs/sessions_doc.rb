d72db42b1024c527e7caacc6cc8c695552f1069a
Luca Tironi <luca.tironi@gmail.com>
2015-12-04 15:09:26 +0100

    Typo in doc

--- app/docs/sessions_doc.rb
+++ app/docs/sessions_doc.rb
@@ -15,7 +15,7 @@ module SessionsDoc
     description 'Creates a new session if the user credentials are correct'
     param :user, Hash, required: true, action_aware: true, desc: 'User credentials' do
       param :email, String, 'Email address of the user', required: true
-      param :password, String, 'Passwordr of the user', required: true
+      param :password, String, 'Password of the user', required: true
     end
   end
 
